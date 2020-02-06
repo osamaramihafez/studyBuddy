f2dd0ebbdae5b6c969960f82a5f67d5d83660033
unknown <anthony.chu@mail.utoronto.ca>
2020-02-05 22:07:22 -0500

    Add Task

---
+++ src/models/Task.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
+require('../middleware/auth')
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        Title: String,
+        Description: String,
+        Completed: Boolean
+    }
+    // timestamps: true
+})
+
+userSchema.methods.genJWT = async function () {
+    const user = this;
+    const token = jwt.sign({
+      _id: user._id.toString()
+    }, process.env.JWTS);
+    user.tokens = user.tokens.concat({
+      token
+    });
+    await user.save();
+    return token;
+  };
+
+// Encypts the password before its pushed to the DB.
+// Saving passwords in plain text is never a good idea
+userSchema.pre("save", async function (next) {
+    const user = this;
+    if (user.isModified("password")) {
+      user.password = await bcrypt.hash(user.password, 8);
+    }
+    next();
+  });
+userSchema.statics.validatePass = async (pw) => {
+    const user = this;
+    const isValid = await bcrypt.compare(pw, user.password)
+    return isValid;
+}
+
+userSchema.statics.login = async (email, password) => {
+    const user = await this.findOne({email});
+    if(!user) throw new Error("Unable to login");
+    const isValid = await user.validatePass(password);
+    if(!isValid) throw new Error("Unable to login");
+    else return user;
+}
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User
