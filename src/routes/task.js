7e61470e869c823c6824e49061da547d7365aa62
Gagandeep Bhangal <gagandeep.bhangal@mail.utoronto.ca>
2020-02-06 16:06:01 -0500

    [STUD-3] Added the Create, Read, Update, and Delete functions for a Task

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,27 +1,56 @@
 const router = require('express').Router();
 let task = require('../models/task');
 
+//Create a task
 router.route('/create/task').post((req, res) => {
     const title = req.body.title;
     const description = req.body.description;
     const completed = req.body.completed;
-})
+    const newTask = new task({
+        title,
+        description,
+        completed
+    })
 
-// Create an account
-router.post('/create/task', async (req, res, next) => {  
-    const user = new User(req.body);
-    console.log(user);
-    try {
-        const tk = await user.genJWT();
-        await user.save();
-        res.status(201).send({
-            user,
-            tk
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
-    }  
+    newTask.save()
+    .then(() => res.json('Task added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Output all tasks
+router.route('/').get((req, res) => {
+    task.find()
+    .then(task => res.json(task))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Output a SPECIFIC task by its id
+router.route('/:id').get((req, res) => {
+    task.findById(req.params.id)
+    .then(task => res.json(task))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Delete a task by using a delete command and sending in the id
+router.route('/:id').delete((req, res) => {
+    task.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Task Deleted!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//Update a task by sending in its id and using a post command
+router.route('/update/:id').post((req, res) => {
+    task.findById(req.params.id)
+    .then(task => {
+        task.title = req.body.title;
+        task.description = req.body.description;
+        task.completed = req.body.completed;
+
+        task.save()
+        .then(() => res.json('Task Updated!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router
