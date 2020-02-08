e3219c4c3d2991515b7fd40e2120d5df456b2f3e
Anthony Chu <anthony.chu@mail.utoronto.ca>
2020-02-08 09:38:36 -0500

    Update Task

---
+++ src/routes/Task.js
@@ -0,0 +1,7 @@
+const auth = require('../middleware/auth')
+
+router.post('/create/task', auth, async (req, res, next) => ***REMOVED***  
+***REMOVED***
+
+router.post('/delete/task', auth, async (req, res, next) => ***REMOVED***  
+***REMOVED***
