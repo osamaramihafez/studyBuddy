9013283a7e876faace5aed953082d414aa0e96bc
Asadali2020 <duale.ali@mail.utoronto.ca>
2020-02-13 15:08:55 -0500

    Successful working timer with start and stop, reset is bugged.

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import '../App.css';
-import Timer from './Timer';
+import React from 'react'
+import logo from './logo.svg'
+import '../App.css'
+import Timer from './Timer'
 
 class App extends React.Component {
-  constructor(){
-    super();
+  constructor() {
+    super()
 
     this.state = {
-      timerMinute : 25,
+      timerMinute: 25,
     }
+    this.updateTimer = this.updateTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Want to see the timer here. {/* Want to put the timer here on the html. */}
-          <h2>
-          <Timer timerMinute={this.state.timerMinute}/>
-          </h2>
-        </p>
-        POMODORO TIMER.
-       {/* <a
+
+  updateTimer() {
+    this.setState( (prevState) => {
+      return { 
+        timerMinute: prevState.timerMinute - 1 
+      }
+    })
+  }
+
+  resetTimer() {
+    this.setState({
+      timerMinute: 25,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" /> {/* Instead of image here, it can be the task. */}
+          <p>
+            <Timer 
+              timerMinute={this.state.timerMinute} 
+              updateTimer={this.updateTimer}
+              resetTimer={this.resetTimer}
+            />
+          </p>
+          POMODORO TIMER.
+          {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -31,10 +49,10 @@ class App extends React.Component {
         >
           POMODORO TIMER
        </a>*/}
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
