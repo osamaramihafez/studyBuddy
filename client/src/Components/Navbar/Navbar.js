e1ead2475c8a6b5ce6721f92924e30d3b392d281
Ahmad Fakhry <ahmad.fakhry@mail.utoronto.ca>
2020-02-12 22:29:21 -0500

    Base for [STUD-19] [STUD-20] part of [STUD-1]

---
+++ client/src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    Navbar,
+  } from 'reactstrap';
+
+const Navi = () => (
+    <div>
+        <Navbar expand="lg" variant="light" bg="light">
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+        </Navbar>
+    </div>
+)
+export default Navi;
