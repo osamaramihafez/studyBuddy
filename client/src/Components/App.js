409928684fba183f964aff950c346cc6c0824432
Asadali2020 <duale.ali@mail.utoronto.ca>
2020-02-13 15:08:55 -0500

    Successful working timer with start and stop, reset is bugged.

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,29 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import '../App.css';
-import Timer from './Timer';
+import React from 'react'
+import logo from './logo.svg'
+import '../App.css'
+import Timer from './Timer'
 
 class App extends React.Component ***REMOVED***
-  constructor()***REMOVED***
-    super();
+  constructor() ***REMOVED***
+    super()
 
     this.state = ***REMOVED***
-      timerMinute : 25,
+      timerMinute: 25,
     }
+    this.updateTimer = this.updateTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
   }
-  render()***REMOVED***
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src=***REMOVED***logo} className="App-logo" alt="logo" />
-        <p>
-          Want to see the timer here. ***REMOVED***/* Want to put the timer here on the html. */}
-          <h2>
-          <Timer timerMinute=***REMOVED***this.state.timerMinute}/>
-          </h2>
-        </p>
-        POMODORO TIMER.
-       ***REMOVED***/* <a
+
+  updateTimer() ***REMOVED***
+    this.setState( (prevState) => ***REMOVED***
+      return ***REMOVED*** 
+        timerMinute: prevState.timerMinute - 1 
+      }
+    })
+  }
+
+  resetTimer() ***REMOVED***
+    this.setState(***REMOVED***
+      timerMinute: 25,
+    })
+  }
+
+  render() ***REMOVED***
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src=***REMOVED***logo} className="App-logo" alt="logo" /> ***REMOVED***/* Instead of image here, it can be the task. */}
+          <p>
+            <Timer 
+              timerMinute=***REMOVED***this.state.timerMinute} 
+              updateTimer=***REMOVED***this.updateTimer}
+              resetTimer=***REMOVED***this.resetTimer}
+            />
+          </p>
+          POMODORO TIMER.
+          ***REMOVED***/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -31,10 +49,10 @@ class App extends React.Component ***REMOVED***
         >
           POMODORO TIMER
        </a>*/}
-      </header>
-    </div>
-  );
-}
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
