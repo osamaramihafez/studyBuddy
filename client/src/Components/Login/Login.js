f4fca19cd6b34e334cce7a7cfcdf1dbbf05006b6
Ahmad Fakhry <afakhry98@gmail.com>
2020-02-13 23:02:54 -0500

    CSS Styling done for [STUD-19] part of [STUD-1]

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,22 +1,57 @@
-import React from 'react'
-import ***REMOVED***
-  Button,
-  } from 'reactstrap';
+import React from "react";
+import ***REMOVED*** FormGroup, FormControl } from "react-bootstrap";
+import Button from 'react-bootstrap/Button'
+import "./Login.css";
 
 class LoginForm extends React.Component ***REMOVED***
+  constructor(props) ***REMOVED***
+    super();
+  }
+
+  state = ***REMOVED***
+    email: "",
+    password: "",
+  }
+
+  validateForm() ***REMOVED***
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit(event) ***REMOVED***
+    event.preventDefault();
+  }
+
+  updatePassword(password) ***REMOVED***
+    this.setState(***REMOVED*** password ***REMOVED***
+  }
+
+  updateEmail(email) ***REMOVED***
+    this.setState(***REMOVED*** email ***REMOVED***
+  }
   render() ***REMOVED***
     return (
-      <div>
-        <form>
-            <label>
-                <input type="text" id="emailInp" placeholder="email" />
-                <input type="password" id="pwdInp" placeholder="password" />
-            </label>
-            <hr></hr>
-            <Button color="primary">Login</Button>
+      <div className="Login">
+        <form onSubmit=***REMOVED***this.handleSubmit}>
+          <FormGroup controlId="email" className="emailInp">
+            <FormControl
+              autoFocus
+              type="email"
+              onChange=***REMOVED***e => this.updateEmail(e.target.value)}
+              placeholder="email"
+            />
+          </FormGroup>
+          <FormGroup controlId="password" className="pwdInp">
+            <FormControl
+              onChange=***REMOVED***e => this.updatePassword(e.target.value)}
+              type="password"
+              placeholder="password"
+            />
+          </FormGroup>
+          <Button variant="outline-secondary">Button</Button>
         </form>
-      </div>
-    )
+      </div >
+    );
   }
 }
-export default LoginForm
+
+export default LoginForm;
