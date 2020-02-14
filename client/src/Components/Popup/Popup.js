a22e3a3e4e8aa1956ff244b9d7b280ab3e4c5822
Ahmad Fakhry <afakhry98@gmail.com>
2020-02-14 18:35:57 -0500

    Completed [STUD-20]

--- client/src/Components/Popup/Popup.js
+++ client/src/Components/Popup/Popup.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import LoginForm from '../Login/Login'
-// import RegisterationFrom from '../Register/Register.js'
+import RegisterationFrom from '../Register/Register.js'
 import './Popup.css'
 
 class Navi extends React.Component ***REMOVED***
@@ -9,7 +9,8 @@ class Navi extends React.Component ***REMOVED***
       <div className="popup">
         <h1 className="title">Welcome</h1>
         <h2 className="subtitle">StudyBuddy</h2>
-        <LoginForm></LoginForm>
+        ***REMOVED***/* <LoginForm></LoginForm> */}
+        <RegisterationFrom></RegisterationFrom>
         <button className="special-link">Not registered? Register here</button>
       </div>
     )
--- client/src/Components/Register/Register.js
+++ client/src/Components/Register/Register.js
@@ -24,13 +24,12 @@ class RegistrationForm extends React.Component ***REMOVED***
 
     handleRegistration(e, state) ***REMOVED***
         e.preventDefault();
-        console.log(state);
     }
     render() ***REMOVED***
         return (
             <div className="Register">
                 <form onSubmit=***REMOVED***e => this.handleRegistration(e, this.state)}>
-                    <FormGroup controlId="name" className="emailInp">
+                    <FormGroup controlId="name" className="inp-top">
                         <FormControl
                             autoFocus
                             type="text"
@@ -38,21 +37,20 @@ class RegistrationForm extends React.Component ***REMOVED***
                             placeholder="name"
                         />
                     </FormGroup>
-                    <FormGroup controlId="email" className="emailInp">
+                    <FormGroup controlId="email" className="inp-mid">
                         <FormControl
                             type="email"
                             onChange=***REMOVED***e => this.updateEmail(e.target.value)}
                             placeholder="email"
                         />
                     </FormGroup>
-                    <FormGroup controlId="password" className="pwdInp">
+                    <FormGroup controlId="password" className="inp-mid">
                         <FormControl
-                            onChange=***REMOVED***e => this.updatePassword(e.target.value)}
                             type="password"
+                            onChange=***REMOVED***e => this.updatePassword(e.target.value)}
                             placeholder="password"
                         />
                     </FormGroup>
-                    <Button variant="outline-secondary" type="submit">Submit</Button>
                 </form>
             </div>
         );
