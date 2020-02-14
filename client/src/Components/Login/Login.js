4ff7c9b77544cfee3faf3847e7b4a3c802cd14f9
Ahmad Fakhry <afakhry98@gmail.com>
2020-02-13 23:02:54 -0500

    CSS Styling done for [STUD-19] part of [STUD-1]

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,22 +1,57 @@
-import React from 'react'
-import {
-  Button,
-  } from 'reactstrap';
+import React from "react";
+import { FormGroup, FormControl } from "react-bootstrap";
+import Button from 'react-bootstrap/Button'
+import "./Login.css";
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+  state = {
+    email: "",
+    password: "",
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  updatePassword(password) {
+    this.setState({ password });
+  }
+
+  updateEmail(email) {
+    this.setState({ email });
+  }
   render() {
     return (
-      <div>
-        <form>
-            <label>
-                <input type="text" id="emailInp" placeholder="email" />
-                <input type="password" id="pwdInp" placeholder="password" />
-            </label>
-            <hr></hr>
-            <Button color="primary">Login</Button>
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId="email" className="emailInp">
+            <FormControl
+              autoFocus
+              type="email"
+              onChange={e => this.updateEmail(e.target.value)}
+              placeholder="email"
+            />
+          </FormGroup>
+          <FormGroup controlId="password" className="pwdInp">
+            <FormControl
+              onChange={e => this.updatePassword(e.target.value)}
+              type="password"
+              placeholder="password"
+            />
+          </FormGroup>
+          <Button variant="outline-secondary">Button</Button>
         </form>
-      </div>
-    )
+      </div >
+    );
   }
 }
-export default LoginForm
+
+export default LoginForm;
--- client/src/Components/Popup/Popup.js
+++ client/src/Components/Popup/Popup.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import LoginForm from '../Login/Login'
-import classes from './Popup.css'
+import './Popup.css'
 
 class Navi extends React.Component {
   render() {
     return (
-      <div className={classes.Popup}>
-        <h1 className="display-3">Welcome</h1>
-        <h4 className="display-3">StudyBuddy</h4>
+      <div className="popup">
+        <h1 className="title">Welcome</h1>
+        <h2 className="subtitle">StudyBuddy</h2>
         <LoginForm></LoginForm>
+        <a href="#" className="special-link">Not registered? Register here</a>
       </div>
     )
   }
