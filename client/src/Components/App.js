972c9d01968d016ba74a2dd9815ec21a251f37c9
unknown <anthony.chu@mail.utoronto.ca>
2020-02-26 20:02:41 -0500

    [STUD-8] Complete [STUD-8]

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import logo from './logo.svg'
 import '../App.css'
 import Timer from './Timer'
+import Fullscreen from "react-full-screen";
+
+//Fullscreen inspired from: https://www.npmjs.com/package/react-full-screen
 
 class App extends React.Component {
   constructor() {
@@ -9,11 +12,14 @@ class App extends React.Component {
 
     this.state = {
       timerMinute: 25,
+      isFull:false,
     }
     this.updateTimer = this.updateTimer.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
   }
-
+  goFull = () => {
+    this.setState({ isFull: true });
+  }
   updateTimer() {
     this.setState( (prevState) => {
       return {
@@ -30,7 +36,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+        <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" /> {/* Instead of image here, it can be the task. */}
           <p>
@@ -41,6 +47,10 @@ class App extends React.Component {
             />
           </p>
           POMODORO TIMER.
+          <button onClick={this.goFull}>
+          Go Fullscreen
+        </button>
+
           {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -50,6 +60,36 @@ class App extends React.Component {
           POMODORO TIMER
        </a>*/}
         </header>
+        <Fullscreen
+          enabled={this.state.isFull}
+          onChange={isFull => this.setState({isFull})}>
+                    <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" /> {/* Instead of image here, it can be the task. */}
+          <p>
+            <Timer
+              timerMinute={this.state.timerMinute}
+              updateTimer={this.updateTimer}
+              resetTimer={this.resetTimer}
+            />
+          </p>
+          POMODORO TIMER.
+          <button onClick={this.goFull}>
+          Go Fullscreen
+        </button>
+
+          {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          POMODORO TIMER
+       </a>*/}
+        </header>
+          {/* <div className="full-screenable-node">
+            This is now in Fullscreen
+          </div> */}
+        </Fullscreen>
       </div>
     )
   }
