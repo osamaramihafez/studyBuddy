75ef8c9269648895f4ab0533329eee04a3c2baee
Asadali2020 <duale.ali@mail.utoronto.ca>
2020-02-27 00:14:35 -0500

    Finished STUD-44

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import logo from './logo.svg'
 import '../App.css'
 import Timer from './Timer'
+import Break from './Break'
+import Session from './Session'
 
 class App extends React.Component ***REMOVED***
   constructor() ***REMOVED***
@@ -9,10 +11,18 @@ class App extends React.Component ***REMOVED***
 
     this.state = ***REMOVED***
       timerMinute: 25,
+      break: 5,
+      session: 25,
+      flipper: true,
+      counter : false
     }
     this.updateTimer = this.updateTimer.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
+    this.changeBreak = this.changeBreak.bind(this);
+    this.changeSession = this.changeSession.bind(this);
+    this.countdown = this.countdown.bind(this);
   }
+
 
   updateTimer() ***REMOVED***
     this.setState( (prevState) => ***REMOVED***
@@ -20,35 +30,88 @@ class App extends React.Component ***REMOVED***
         timerMinute: prevState.timerMinute - 1
       }
     })
+    
+  }
+
+  countdown(boolean) ***REMOVED***
+    this.setState(***REMOVED***
+      counter: boolean
+    })
   }
 
   resetTimer() ***REMOVED***
     this.setState(***REMOVED***
-      timerMinute: 25
+      timerMinute: 25,
+      break: 5,
+      session: 25,
     })
   }
+  
+  changeBreak(breaktwo) ***REMOVED***
+
+      if (this.counter === true)***REMOVED***
+        this.setState(***REMOVED***
+          break: breaktwo
+        })
+      }
+      else***REMOVED***
+        this.setState(***REMOVED***
+          counter: false,
+          flipper: false,
+          break: breaktwo,
+          timerMinute: breaktwo
+        })
+      }
+    
+     
+    
+    
+  }
+
+  changeSession(newsession) ***REMOVED***
+    
+    if (this.counter === true)***REMOVED***
+      this.setState(***REMOVED***
+        session: newsession
+      })
+    }
+    else***REMOVED***
+      this.setState(***REMOVED***
+        counter: false,
+        flipper: true,
+        session: newsession,
+        timerMinute: newsession
+      })
+    }
+  }
+
 
   render() ***REMOVED***
     return (
       <div className="App">
         <header className="App-header">
           <img src=***REMOVED***logo} className="App-logo" alt="logo" /> ***REMOVED***/* Instead of image here, it can be the task. */}
+          <h4> ***REMOVED*** this.state.flipper === true ? "Session": "Break" } </h4>
           <p>
             <Timer
               timerMinute=***REMOVED***this.state.timerMinute}
               updateTimer=***REMOVED***this.updateTimer}
               resetTimer=***REMOVED***this.resetTimer}
+              countdown=***REMOVED***this.countdown}
+              break = ***REMOVED***this.break}
             />
+            <section className="interval-container">
+            <Break
+            changeBreak = ***REMOVED***this.changeBreak}
+            break = ***REMOVED***this.state.break}
+            />
+            <Session
+            changeSession = ***REMOVED***this.changeSession}
+            session = ***REMOVED***this.state.session}
+            />
+            </section>
           </p>
           POMODORO TIMER.
-          ***REMOVED***/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          POMODORO TIMER
-       </a>*/}
         </header>
       </div>
     )
