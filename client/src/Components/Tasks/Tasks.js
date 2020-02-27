f6b867d3ec3a95e55f41a746d8504280966fd22c
Admin <admin@Admins-MacBook-Air.local>
2020-02-27 00:58:14 -0500

    Completed CSS, and js for add/del group, add/del task

--- client/src/Components/Tasks/Tasks.js
+++ client/src/Components/Tasks/Tasks.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Tasks.css'
 
 class Tasks extends React.Component {
     constructor() {
@@ -15,23 +16,25 @@ class Tasks extends React.Component {
 
     addTask() {
         //create item with unique id
-        const appendTask={
-          id: 1 + Math.random(),
-          value: this.state.newTask.valueOf(),
-          group: this.state.chosenGroup.valueOf()
-        };
-    
-        //copy of current list of items
-        const list = [...this.state.taskList]
-    
-        //add new item to list
-        list.push(appendTask)
-    
-        //update state with new list and reset newItem input
-        this.setState({
-          newItem:"",
-          taskList:list
-        });
+        if(this.state.newTask.trim()){
+            const appendTask={
+            id: 1 + Math.random(),
+            value: this.state.newTask.trim().valueOf(),
+            group: this.state.chosenGroup.valueOf()
+            };
+        
+            //copy of current list of items
+            const list = [...this.state.taskList]
+        
+            //add new item to list
+            list.push(appendTask)
+        
+            //update state with new list and reset newItem input
+            this.setState({
+                newTask:"",
+                taskList:list
+            });
+        }
     }
     
     updateTaskInput(value) {
@@ -41,16 +44,19 @@ class Tasks extends React.Component {
     }
 
     addGroup() {
-    
-        //copy of current list of items
-        const list = [...this.state.groupsList]
+        if(this.state.newGroup.trim()) {
+            //copy of current list of items
+            const list = [...this.state.groupsList]
 
-        list.push(this.state.newGroup)
-        //update state with new list and reset newItem input
-        this.setState({
-          newGroup:"",
-          groupsList:list
-        });
+            
+            list.push(this.state.newGroup.trim())
+            
+            //update state with new list and reset newItem input
+            this.setState({
+                newGroup:"",
+                groupsList:list
+            });
+        }   
     }
     
     updateGroupInput(value) {
@@ -76,68 +82,96 @@ class Tasks extends React.Component {
     }
 
     deleteGroup(group) {
-        //todo
+        const list = [...this.state.taskList]
+
+        const updatedList = list.filter(item => item.group!==group);
+
+        const groupsList = [...this.state.groupsList]
+
+        const updatedGroupsList = groupsList.filter(item => item!==group)
+
+        this.setState({
+            taskList:updatedList,
+            groupsList:updatedGroupsList,
+        }); 
+    }
+
+    filterGroup(group) {
+
     }
 
     render() {
         return (
-            <div class="tasksList">
-                <ul>
-                    {this.state.groupsList.map(group => {
-                        return (
-                        <li>
-                            {group} 
-                            <button onClick={() => this.deleteGroup(group)}>
-                                -
-                            </button>
-                        </li>
-                        )
-                    })}
-                </ul>
-                <button onClick={() => this.addGroup()}>
-                +
-                </button>
-                <input 
-                    type="text"
-                    placeholder="Add Group"
-                    value={this.state.newGroup}
-                    onChange={e => this.updateGroupInput(e.target.value)}
-                />
-                <ul>
-                    {this.state.taskList.map(task => {
-                        return (
-                        <li key={task.id}>
-                            <p>
-                                {task.value} - {task.group}
-                                <button onClick={() => this.deleteTask(task.id)}>
-                                -
-                                </button>
-                            </p>
-                            
-                        </li>
-                        )
-                    })}
-                </ul>
-                <button onClick={() => this.addTask()}>
-                +
-                </button>
-                <input 
-                    type="text"
-                    placeholder="Add task"
-                    value={this.state.newTask}
-                    onChange={e => this.updateTaskInput(e.target.value)}
-                />
-                <select name="Groups" onChange={e => this.chooseGroup(e.target.value)}>
-                    <option value="" disabled selected hidden>Groups</option>
-                    {this.state.groupsList.map(group => {
-                        return (
-                        <option value={group}>
-                            {group}
-                        </option>
-                        )
-                    })}
-                </select>
-                
+            <div className="taskClassContainer">
+                <div className="groupsContainer">
+                    <h1>Groups</h1>
+                    <br/>
+                    <ul>
+                        {this.state.groupsList.map(group => {
+                            return (
+                            <li>
+                                {group}<p> </p>
+                                <select className="groupNames">
+                                    <option value="" disabled selected hidden>...</option>
+                                    <option value="Filter" onClick={() => this.filterGroup(group)}>Filter</option>
+                                    <option value="Delete" onClick={() => this.deleteGroup(group)}>Delete</option>
+                                </select>
+                            </li>
+                            )
+                        })}
+                    </ul>
+                    <div className="groupInput">
+                        <button className="addButton" onClick={() => this.addGroup()}>
+                            +
+                        </button>
+                        <input 
+                            type="text"
+                            placeholder="Add Group"
+                            value={this.state.newGroup}
+                            onChange={e => this.updateGroupInput(e.target.value)}
+                        />
+                    </div>
+                </div>
+                <div className="tasksContainer">
+                    <h1>Tasks</h1>
+                    <br/>
+                    <ul>
+                        {this.state.taskList.map(task => {
+                            return (
+                            <li key={task.id}>
+                                <p>
+                                    {task.value} - {task.group}
+                                    <button onClick={() => this.deleteTask(task.id)}>
+                                    -
+                                    </button>
+                                </p>
+                                
+                            </li>
+                            )
+                        })}
+                    </ul>
+                    <div className = "taskInput">
+                        <button className="addButton" onClick={() => this.addTask()}>
+                        +
+                        </button>
+                        <input 
+                            type="text"
+                            placeholder="Add task"
+                            value={this.state.newTask}
+                            onChange={e => this.updateTaskInput(e.target.value)}
+                        />
+                        <select name="Groups" onChange={e => this.chooseGroup(e.target.value)}>
+                            <option value="" disabled selected hidden>Groups</option>
+                            {this.state.groupsList.map(group => {
+                                return (
+                                <option value={group}>
+                                    {group}
+                                </option>
+                                )
+                            })}
+                        </select>
+                    </div>
+                </div>
             </div>  
         )
     }
