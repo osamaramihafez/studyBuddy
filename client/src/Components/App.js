650f59a22ad07a441281b1bb75bdb82926774ef5
Shaun Aranha <raphaelaranha@Shauns-MacBook-Pro.local>
2020-02-27 00:48:06 -0500

    Finished STUD-36 and STUD-37 which was the implementation of a permanent session list and a function to move selected tasks to the list

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import '../App.css';
 import Timer from './Timer';
+import SessionList from './SessionList';
+
 
 class App extends React.Component ***REMOVED***
   constructor()***REMOVED***
@@ -32,6 +34,7 @@ class App extends React.Component ***REMOVED***
           POMODORO TIMER
        </a>*/}
       </header>
+      <SessionList />
     </div>
   );
 }
