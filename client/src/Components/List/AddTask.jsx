14cd02966a7178172dd94e6337cf1f2956cf1e92
Shaun Aranha <raphaelaranha@Shauns-MacBook-Pro.local>
2020-03-28 00:11:46 -0400

    STUD-107 still being worked on as functionality issues are still persistent

---
+++ client/src/Components/List/AddTask.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import axios from 'axios'
+import { Draggable } from 'react-beautiful-dnd';
+import Button from 'react-bootstrap/Button';
+import Task from './task';
+
+const Container = styled.div`
+  border: 1px solid lightgrey;
+  border-radius: 2px;
+  padding: 8px;
+  margin-bottom: 8px;
+  background-color: ${props => (props.isDragging ? 'lightblue' : 'white')};
+`;
+
+
+
+export default class AddTask extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      text: ' '
+    };
+    this.changeText = this.changeText.bind(this);
+  }
+
+  changeText(e){
+    this.setState({text: e.target.value});
+  }
+
+
+
+
+  render() {
+    return (
+          <Container
+          >
+            <input type="text" value={this.state.text} id="newtask" onChange={this.changeText} />
+            <Button variant="outline-primary" onClick={this.props.addTask.bind(this.state.text,this.state.id)}>Add Task</Button>
+          </Container>
+
+    );
+  }
+}
